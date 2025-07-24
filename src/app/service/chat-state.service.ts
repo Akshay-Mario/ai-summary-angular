464e3f928ea1b6dfdcdb7ee7683f72bc7d0ab209
Akshay-Mario <akshay1997qw@gmail.com>
2025-07-25 01:10:37 +0530

    moving chat service inside service folder

--- src/app/service/chat-state.service.ts
+++ src/app/service/chat-state.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { IChatState } from './shared/models/chat-state.model';
+import { IChatState } from '../shared/models/chat-state.model';
 
 @Injectable({
   providedIn: 'root'
